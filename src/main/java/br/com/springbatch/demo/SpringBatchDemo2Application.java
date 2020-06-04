3993562dc9fbf5b690054a7ce26bdaa0371a8550
Fábio Kopezinski <fabiokopezinski@gmail.com>
2020-06-04 10:52:08 -0300

    commit initial

---
+++ src/main/java/br/com/springbatch/demo/SpringBatchDemo2Application.java
@@ -0,0 +1,13 @@
+package br.com.springbatch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springbatch/demo/SpringBatchDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbatch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
